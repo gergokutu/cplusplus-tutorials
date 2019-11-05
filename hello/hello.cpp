49a6554ad63a101c98f57b6bead29e402358bce8
gergokutu <gergo.kutu@gmail.com>
2019-11-05 20:50:05 +0100

    Hello World in C++

---
+++ hello/hello.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// // using namespace std;
+
+// int main()
+// {
+//   std::cout << "Hello World\n";
+//   return 0;
+// }
+
+// every run (terminal » g++ hello.cpp)
+// creates » a.out (an executable, a c++ application)
+// terminal » ./a.out
+// Hello World
+
+// use the namespace
+// std::cout » cout
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World\n";
+  return 0;
+}
