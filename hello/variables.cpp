5fd7454435d0206e6363c8e74176336ebfd4c713
gergokutu <gergo.kutu@gmail.com>
2019-11-06 13:57:30 +0100

    Variables

---
+++ hello/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{ 
+  // declaration and initialization
+  // int slices;
+  // slices = 5;
+  int slices = 5 + 2; // int is the datatype
+  int children = slices;
+  slices = 1000; // children remain the same
+
+  cout << "HELLOOOO\n";
+  cout << children;
+  // return 0; // optional line
+}
