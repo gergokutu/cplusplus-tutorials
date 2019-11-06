c0eae17204b4a027eb5413f3c6275fdb6fa1047a
gergokutu <gergo.kutu@gmail.com>
2019-11-06 20:39:21 +0100

    Variables, cout, printf()

--- hello/variables.cpp
+++ hello/variables.cpp
@@ -11,6 +11,16 @@ int main()
   slices = 1000; // children remain the same
 
   cout << "HELLOOOO\n";
-  cout << children;
+  cout << children << "\n";
+  cout << slices;
+  // << std::endl » new line like << "\n"
+  cout << "You have " << slices << " slices of pizza." << std::endl;
+
+  // printf comes from C language
+  // you have to say what format it has
+  // it's gonna be a string
+  // and this is a integer
+  // and put a new line after it
+  printf("%i\n", slices); // alternative for cout, but cout is easier to use
   // return 0; // optional line
 }
