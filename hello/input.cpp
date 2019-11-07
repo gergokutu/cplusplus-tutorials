993093c04eab955449884510bed9018952e2bd88
gergokutu <gergo.kutu@gmail.com>
2019-11-07 13:47:38 +0100

    User input with cin >>

---
+++ hello/input.cpp
@@ -0,0 +1,17 @@
+// include the file iostream as preprocessed codes...
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+  int slices = 10;
+  cout << "How many slice(s) of pizza do you want? ";
+  // consol in
+  // cin is an instance of istream
+  // cout is an instance of ostream
+  cin >> slices;
+  cout << "You have " << slices << " slice(s) of pizza." << std::endl;
+  // return 0;
+}
