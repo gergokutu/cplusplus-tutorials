cac0d9c5a7601c7615a75fe1035c84ed0923d582
gergokutu <gergo.kutu@gmail.com>
2019-11-08 13:39:07 +0100

    Void function, dry coding, change on 1 place

---
+++ hello/function_void.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+
+double power(double base, int exponent)
+{
+  double result = 1;
+  for (int i = 0; i < exponent; i++)
+  {
+    result = result * base;
+  }
+  return result;
+}
+
+// print the result to the consol
+void print_pow(double base, int exponent)
+{
+  double myPower = power(base, exponent);
+  cout << base <<  " raised to the " << exponent << " power is " << myPower << ".\n";
+}
+
+int main()
+{ 
+  double base;
+  int exponent;
+  cout << "What is the base? ";
+  cin >> base;
+  cout << "What is the exponent? ";
+  cin >> exponent;
+  print_pow(base, exponent);
+  // benefit » do not have to repeat the codes just call
+  print_pow(3, 3);
+}
