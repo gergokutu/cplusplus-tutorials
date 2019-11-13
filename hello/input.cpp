c75cdf06e536923d5c4f2d3780c52cc42d51692f
gergokutu <gergo.kutu@gmail.com>
2019-11-13 16:45:51 +0100

    String class, C string

--- hello/input.cpp
+++ hello/input.cpp
@@ -10,8 +10,8 @@ int main()
   cout << "How many slice(s) of pizza do you want? ";
   // consol in
   /* 
-    cin is an instance of istream
-    cout is an instance of ostream
+    cin is an instance of input stream
+    cout is an instance of output stream
   */
   cin >> slices;
   cout << "You have " << slices << " slice(s) of pizza." << std::endl;
---
+++ hello/strings.cpp
@@ -0,0 +1,43 @@
+// String class vs C style strings
+#include <iostream>
+// get access to the string class
+#include <string>
+
+int main()
+{ 
+  // creating an object from string class
+  // string is characters that strand together
+  std::string greeting = "Hello";
+  std::cout << greeting[0] << std::endl;
+
+  std::string name; // "" >> an e mpty string is the default value
+  std::cout << name << std::endl;
+
+  // string concatenation
+  std::cout << "Type your name: ";
+  // cin grabs only the first word
+  // no Gergo Kovacs, just Gergo
+  std::cin >> name;
+  std::cout << "Hello, " + name + "!" << std::endl;
+
+  std::string complete_greeting = greeting + " there";
+  complete_greeting += "!";
+  std::cout << complete_greeting << std::endl;
+
+  // members >> there are functions tied to the class (method)
+  std::cout << complete_greeting.length() << std::endl;
+
+  // example of a C string
+  // more complicated
+  // array of characters >> works, but has limitations
+  // e.g. string size is not easily changeable
+  char car[] = "Lada";
+  // error >> Ferrari is longer, needs more memory space...
+  // name[] = "Ferrari";
+  // nor a shorter string is allowed
+  // name[] = "BMW";
+
+  std::string bye;
+  std::cin >> bye;
+  std::cout << bye << std::endl;
+}
