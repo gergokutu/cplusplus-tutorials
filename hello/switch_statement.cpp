3f82e6d66b8bcc3a737e9d9a1eb20c54b6008a8f
gergokutu <gergo.kutu@gmail.com>
2019-11-18 13:55:20 +0100

    Switch structure

--- hello/switch_statement.cpp
+++ hello/switch_statement.cpp
@@ -4,6 +4,23 @@
 
 int main()
 {
-  
+  int age;
+  std::cout << "Your age: ";
+  std::cin >> age;
+  switch(age)
+  {
+    case 12:
+      std::cout << "Mommy's little boy" << std::endl;
+      break;
+    case 18:
+      std::cout << "Do not forget your driving licence!" << std::endl;
+      break;
+    case 35:
+      std::cout << "Welcome junior developer\n";
+      break;
+    default:
+      std::cout << "No match with this age\n";
+      break;
+  }
   return 0;
 }
