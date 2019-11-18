2d9363091fdae032c8c755972c1e8ed01f7e8c7d
gergokutu <gergo.kutu@gmail.com>
2019-11-18 12:28:34 +0100

    Branching basics: if vs switch

---
+++ hello/control_flow.cpp
@@ -0,0 +1,37 @@
+// in the following videos we are going through
+// the key control flow structures
+// 2 main:
+// branching >> if (else if, else), switch (case, default, break)
+// looping >> while, for, do-while
+#include <iostream>
+
+int main()
+{
+  if (expression) // true/false...
+  {
+    // code
+  } 
+  else if (expression_2)
+  {
+    // code
+  } 
+  else
+  {
+    // code
+  }
+
+  // age >> has to be an integral type
+  // javascript >> it also can be a string...
+  switch(age) 
+  {
+    case 17:
+      // code
+      break;
+    case 18:
+      // code
+      break;
+    default:
+      // code
+      break;
+  }
+}
