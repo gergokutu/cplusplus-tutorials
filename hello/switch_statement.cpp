e609c244aac3c3401902b10a0003858e53874ee9
gergokutu <gergo.kutu@gmail.com>
2019-11-18 14:09:37 +0100

    Switch with enumeration

--- hello/switch_statement.cpp
+++ hello/switch_statement.cpp
@@ -7,10 +7,13 @@ int main()
   int age;
   std::cout << "Your age: ";
   std::cin >> age;
+  
   switch(age)
   {
     case 12:
       std::cout << "Mommy's little boy" << std::endl;
+      // done with this case, without >> falls through
+      // works with return 0; as well
       break;
     case 18:
       std::cout << "Do not forget your driving licence!" << std::endl;
@@ -21,6 +24,28 @@ int main()
     default:
       std::cout << "No match with this age\n";
       break;
+  }
+
+  // enum used in a switch statement
+  // enum stores possible values as integral types
+  enum season{summer, spring, fall, winter};
+  season now = winter;
+
+  switch (now)
+  {
+    case summer:
+      break;
+    case spring:
+      break;
+    case fall:
+      break;
+    case winter:
+      std::cout << "winter is cold in Enumerania...\n";
+      break;
+    // default not necessary
+    // because enum we can have only 4 possible values...
+    // default:
+    //   break;
   }
   return 0;
 }
