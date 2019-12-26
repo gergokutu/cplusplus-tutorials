e454b634edde7534491b932640ebd829ce9e9c5b
gergokutu <gergo.kutu@gmail.com>
2019-12-26 14:47:11 +0100

    Print out only the input values of the array in 037_arrays_fill_from_input.cpp

--- 037_arrays_fill_from_input.cpp
+++ 037_arrays_fill_from_input.cpp
@@ -17,10 +17,44 @@ int main()
   // undefined results in crazy values...
   // int guesses[100];
   // to get 100 pcs of 0 >> initialize a empty array
-  int guesses[100] = {};
-  // int guesses[] = {12, 43, 23, 43, 23};
-  int size = sizeof(guesses) / sizeof(int);
-  print_array(guesses, size);
+  // int guesses[100] = {};
+  // int guesses[100] = {12, 43, 23, 43, 23};
+
+  // with statically sized arrays
+  // use bigger size than needed (e.g 10 vs 100)
+  // it has effect on the for loop!!!
+  // because you want to stop giving inputs before 100 iteration
+  // solve this with an if statement inside of the for-loop
+  const int SIZE = 100;
+  int guesses[SIZE];
+  // int size = sizeof(guesses) / sizeof(int);
+
+  int count = 0;
+  
+  for (int i = 0; i < SIZE; i++)
+  {
+    if (std::cin >> guesses[i]) // return cin >> evaluates true if input is OK
+    {
+      // input worked
+      count++;
+    }
+    else
+    {
+      // invalid character >> e.g. q
+      // enter, space, tab... is not ok for this...
+      break;
+    }
+  }
+
+  // this way it prints out the total array
+  // we need a seperate array above >> int count = 0;
+  // increase when the input OK (true)
+  // use count instead of SIZE in the function call
+  // print_array(guesses, SIZE);
+  print_array(guesses, count);
+  // cin keeps the wrong input >> breaks...
+  // correct it
+  std::cin.clear()
 
   return 0;
 }
