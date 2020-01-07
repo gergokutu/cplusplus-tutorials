fb9b8491d68b4b22937ec3b46292532c905c6707
gergokutu <gergo.kutu@gmail.com>
2020-01-07 14:59:03 +0100

    Print out STL array - 042_STL_array.cpp

--- 042_STL_array.cpp
+++ 042_STL_array.cpp
@@ -17,9 +17,19 @@
 // include array
 #include <array>
 
+void print_array(std::array<int, 20> data)
+{
+  for (int i = 0; i < data.size(); i++)
+  {
+    std::cout << data[i] << "\t";
+  }
+  std::cout << "\n";
+}
+
 int main()
 {
   // create an STL array
   // <type, size>
-  std::array<int, 20> data = {1, 2, 3};
+  std::array<int, 20> numbers = {1, 2, 3};
+  print_array(numbers);
 }
