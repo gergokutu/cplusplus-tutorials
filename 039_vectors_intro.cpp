fbe0db037c709df89c0e5edcddfe189cafb5cd10
gergokutu <gergo.kutu@gmail.com>
2020-01-07 10:14:06 +0100

    Add data to the vector, .size() - 039_vectors_intro.cpp

--- 039_vectors_intro.cpp
+++ 039_vectors_intro.cpp
@@ -11,10 +11,13 @@
 // if you pass vector to a function
 // copies the vector into a variable
 // !!! and remembers its size
+// add data to the vector
 // items.push_back(100)
 // items[2]
 // items.size()
 // items[items.size() - 1]
+// vectors are part of c++11 so
+// g++ file_name.cpp -std=c++11
 
 // C++ Programming Tutorial 51 - Creating a Vector
 #include <iostream>
@@ -25,4 +28,7 @@ int main()
 {
   // create a vector and assign some values
   std::vector<int> data = {1, 2, 3};
+  // add data to the vector
+  data.push_back(12);
+  std::cout << data[3] << std::endl;
 }
