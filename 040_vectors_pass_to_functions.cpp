6629cfa45d1cdf7f6f6af19329ddb909bfe7bea4
gergokutu <gergo.kutu@gmail.com>
2020-01-07 10:44:03 +0100

    Print out the vector - 040_vectors_pass_to_functions.cpp

--- 040_vectors_pass_to_functions.cpp
+++ 040_vectors_pass_to_functions.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <vector>
 
+void print_vector(std::vector<int> data)
+{
+  for (int i = 0; i < data.size(); i++)
+  {
+    std::cout << data[i] << "\t";
+  }
+  std::cout << "\n";
+}
+
 int main()
 {
-  std::vector<int> data = {1, 2, 3};
+  std::vector<int> numbers = {1, 2, 3};
+  print_vector(numbers);
 }
