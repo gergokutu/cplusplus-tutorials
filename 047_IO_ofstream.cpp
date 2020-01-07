33b77adeb48cfe315066ed5a49471a8733dd5a7e
gergokutu <gergo.kutu@gmail.com>
2020-01-07 21:45:14 +0100

    Create an object from ofstream - 047_IO_ofstream.cpp

---
+++ 047_IO_ofstream.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// include file stream
+#include <fstream>
+
+int main()
+{
+  // create an object coming from ofstream class
+  // name it file
+  std::ofstream file;
+  return 0;
+}
