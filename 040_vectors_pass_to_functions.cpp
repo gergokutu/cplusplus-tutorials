c82b6f442a403d396da52088ec4a917d380743ca
gergokutu <gergo.kutu@gmail.com>
2020-01-07 10:52:32 +0100

    Change data inside the function >> passing by reference - 040_vectors_pass_to_functions.cpp

--- 040_vectors_pass_to_functions.cpp
+++ 040_vectors_pass_to_functions.cpp
@@ -5,6 +5,21 @@
 // is copied into the data variable (e.g number >> data)
 void print_vector(std::vector<int> data)
 {
+  data.push_back(12);
+  for (int i = 0; i < data.size(); i++)
+  {
+    std::cout << data[i] << "\t";
+  }
+  std::cout << "\n";
+}
+
+// if you want to change the data inside the function
+// you have to pass it by reference
+// &data
+
+void print_vector_ref(std::vector<int> &data)
+{
+  data.push_back(12);
   for (int i = 0; i < data.size(); i++)
   {
     std::cout << data[i] << "\t";
@@ -16,4 +31,10 @@ int main()
 {
   std::vector<int> numbers = {1, 2, 3};
   print_vector(numbers);
+  print_vector(numbers);
+  print_vector(numbers);
+  std::cout << "Passing by reference" << std::endl;
+  print_vector_ref(numbers);
+  print_vector_ref(numbers);
+  print_vector_ref(numbers);
 }
