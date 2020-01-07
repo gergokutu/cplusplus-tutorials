f30527a94e53f119e5f8be59202f722c77df2e40
gergokutu <gergo.kutu@gmail.com>
2020-01-07 10:00:38 +0100

    Create a vector and assign some values - 039_vectors_intro.cpp

---
+++ 039_vectors_intro.cpp
@@ -0,0 +1,28 @@
+// vectors
+// in other languages >> list or array-list
+// similar to arrays but the size is dynamic
+// dynamic collection
+// create a vector
+// <, > (carrots) because vectors is part of the standard template library
+// the next line is a templatized data structure (<int>)
+// ...data_structure<type>...
+// std::vector<int> items = {12, 13}
+// templetized class in C >> generics in C#
+// if you pass vector to a function
+// copies the vector into a variable
+// !!! and remembers its size
+// items.push_back(100)
+// items[2]
+// items.size()
+// items[items.size() - 1]
+
+// C++ Programming Tutorial 51 - Creating a Vector
+#include <iostream>
+// you have to include the vector
+#include <vector>
+
+int main()
+{
+  // create a vector and assign some values
+  std::vector<int> data = {1, 2, 3};
+}
