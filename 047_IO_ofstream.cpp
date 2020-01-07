12654f7c355fcf8246def0162be9ef5c68ff9218
gergokutu <gergo.kutu@gmail.com>
2020-01-07 21:55:38 +0100

    Open/close file - 047_IO_ofstream.cpp

--- 047_IO_ofstream.cpp
+++ 047_IO_ofstream.cpp
@@ -4,8 +4,24 @@
 
 int main()
 {
-  // create an object coming from ofstream class
+  // create an object coming from ofstream class >>
+  // instantiate an object from a class (ofstream)
   // name it file
-  std::ofstream file;
+  // std::ofstream file;
+  // .txt is a naming convention for text files
+  // on Linux/MAC you do not really need the extension
+  // with Windows its a bit different
+  // because the extension determines how the file is compiled
+  // file.open("hello.txt");
+
+  // same but shorter
+  // if the file does not exist
+  // it is automatically created
+  std::ofstream file ("hello.txt");
+
+  // when you are done with the file
+  // actually it is not always necessary
+  // but it explicitly closes the files 
+  file.close();
   return 0;
 }
