c9283285dad37ac4d4c20ceab9730e436fd3ee44
gergokutu <gergo.kutu@gmail.com>
2020-01-07 16:05:33 +0100

    Normal vs range based for-loop - 045_loop_range_based_for.cpp

--- 045_loop_range_based_for.cpp
+++ 045_loop_range_based_for.cpp
@@ -1,8 +1,28 @@
+// the range based for-loop
+// big help when iterating through vectors
 #include <iostream>
 #include <vector>
 #include <array>
 
 int main()
 {
-  
+  int data[] = {1, 2, 3, 4, 5, 6};
+
+  std::cout << "With range based for-loop" << std::endl;
+  // range based for-loop
+  // new variable to store the data of every iteraton (n)
+  // tell where the data come from ( : data)
+  for (int n : data)
+  {
+    std::cout << n << "\t";
+  }
+  std::cout << "\n";
+
+  std::cout << "With normal for-loop" << std::endl;
+  // normal for-loop
+  for (int i = 0; i < 6; i++)
+  {
+    std::cout << data[i] << "\t";
+  }
+  std::cout << "\n";
 }
