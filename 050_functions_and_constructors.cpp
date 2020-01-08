e17a1238e621db1b2c4eccbdb6e04e9e9997b704
gergokutu <gergo.kutu@gmail.com>
2020-01-08 12:31:23 +0100

    Different types of functions - code 050_functions_and_constructors.cpp

---
+++ 050_functions_and_constructors.cpp
@@ -0,0 +1,33 @@
+/*
+In this video >> 5 types of functions
+
+1. function
+  - independent entity
+  - normal functions have a return type
+  - e.g. int, string...
+  - void is a type, too
+
+2. method
+  - funcionality-wise is same as function but
+  - a method is attached to an object
+  - only difference in usage >> how you call them (e.g. obj.func()...)
+
+3. static method
+  - e.g string functions like string.length
+  - a static method is attached to a class
+  - class >> blueprint to how to create objects
+  - every single time when we create an instance of a class (e.g. string class)
+  - we create a new separate object (e.g. std::string name;)
+
+4. Constructor
+  - special function
+  - it's called when instantiating a class
+  - constructor = ctor
+  - does not have a return type
+  - because does not return any data but create an object
+
+5. Destructor
+  - very similar to ctor
+  - called when a object is deleted
+  - clean up all the memory
+*/
