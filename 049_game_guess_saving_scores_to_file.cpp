66da27a1fa19b43bee61b8f6b6f565df7582ec62
gergokutu <gergo.kutu@gmail.com>
2020-01-08 09:39:54 +0100

    Refactor to use vector instead of STL array - 049_game_guess_saving_scores_to_file.cpp

---
+++ 049_game_guess_saving_scores_to_file.cpp
@@ -0,0 +1,73 @@
+// save scores to an external file
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+void print_vector(std::vector<int> vector)
+{
+  for (int i = 0; i < vector.size(); i++)
+  {
+    std::cout << vector[i] << "\t";
+  }
+  std::cout << std::endl;
+  std::cout << "\n";
+}
+
+void play_game()
+{
+  std::vector<int> guesses;
+  int random = rand() % 251;
+  // next line just shows the random number
+  std::cout << random << std::endl;
+  std::cout << "Guess a random number from 0 to 250: " << std::endl;
+
+  while (true)
+  {
+    int guess;
+    std::cin >> guess;
+    
+    guesses.push_back(guess);
+
+    if (guess == random)
+    {
+      std::cout << "\n!!!!!!!!!!!!!!!YOU WIN!!!!!!!!!!!!!!!!\n\n";
+      break;
+    } 
+    else if (guess < random)
+    {
+      std::cout << "Too low\n";
+    } 
+    else
+    {
+      std::cout << "Too high\n";
+    }
+  }
+  
+  print_vector(guesses);
+}
+
+int main()
+{ 
+  srand(time(NULL));
+  int choice;
+
+  do
+  {
+    std::cout << "0. Quit" << std::endl << "1. Play Game\n";
+    std::cin >> choice;
+
+    switch (choice)
+    {
+      case 0:
+        std::cout << "Good bye!\n";
+        return 0;
+      case 1:
+        std::cout << "Yo, let's play a guess game!\n\n";
+        play_game();
+        break;
+    }
+  } 
+  while (choice != 0);
+}
