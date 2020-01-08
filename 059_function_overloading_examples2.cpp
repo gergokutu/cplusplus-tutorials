1fd770d3706f4d7a3cc064a1f1fe201b901ea6fb
gergokutu <gergo.kutu@gmail.com>
2020-01-08 17:54:44 +0100

    2nd area() func - 059_function_overloading_examples2.cpp

--- 059_function_overloading_examples2.cpp
+++ 059_function_overloading_examples2.cpp
@@ -12,14 +12,20 @@ double area(double length, double width)
   return length * width;
 }
 
+double area(double length)
+{
+  return length * length;
+}
+
 int main()
 {
   // create a rectangle
   Rectangle rectangle;
   rectangle.length = 10;
-  rectangle.width = 10;
+  rectangle.width = 20;
 
   std::cout << area(rectangle.length, rectangle.width) << std::endl;
-
+  std::cout << area(rectangle.length) << std::endl;
+  
   return 0;
 }
