512d9af86aa27b9d25f2165f8ad4ace9e570a7ca
gergokutu <gergo.kutu@gmail.com>
2020-01-08 16:54:17 +0100

    Swap_ref function - 056056_pass_by_ref_swap_function.cpp

--- 056_pass_by_ref_swap_function.cpp
+++ 056_pass_by_ref_swap_function.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 
+void swap_ref(int &a, int &b)
+{
+  int temporary = a;
+  a = b;
+  b = temporary;
+
+  std::cout << "Inside >> a: " << a << "\tb: " << b << "\n";
+}
+void swap(int a, int b)
+{
+  // with this both gonna be b
+  // no swap
+  // a = b;
+  // b = a;
+
+  // try this
+  // inside the func >> swapped
+  // outside the func >> not swapped
+  int temporary = a;
+  a = b;
+  b = temporary;
+
+  std::cout << "Inside >> a: " << a << "\tb: " << b << "\n";
+}
+
 int main()
 {
+  int a = 1;
+  int b = 2;
+
+  std::string note1 = "With swap:";
+  std::cout << note1 << std::endl;
+  swap(a, b);
+  std::cout << "Outside >> a: " << a << "\tb: " << b << "\n";
+  std::cout << "\n";
+
+  std::string note2 = "With swap_ref:";
+  std::cout << note2 << std::endl;
+  swap_ref(a, b);
+  std::cout << "Outside >> a: " << a << "\tb: " << b << "\n";
+
   return 0;
 }
