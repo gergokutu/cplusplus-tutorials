4b75cbdc2029260054b9d2b715911c0d17ae7d07
gergokutu <gergo.kutu@gmail.com>
2020-01-09 12:06:55 +0100

    Intro - 063_namespaces_intro.cpp

---
+++ 063_namespaces_intro.cpp
@@ -0,0 +1,27 @@
+/*
+Namespaces
+
+std:: >> standard namespace
+std::string >> string belongs to the standard namespace
+
+- avoid naming conflicts
+
+Creating our own namespaces:
+namespace utilz
+{
+
+}
+- common funcionality >> grouping
+- particular company, project, etc >> grouping
+- classifying your classes/functions
+
+namespace utilz
+{
+  double multiply(double number1, double number 2)
+  {
+    //
+  }
+}
+// call it
+utilz::multiply(1, 2);
+*/
