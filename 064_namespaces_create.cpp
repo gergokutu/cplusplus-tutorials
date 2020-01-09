06c672fc88fe8fab8df935ad13b21959c43f9ee0
gergokutu <gergo.kutu@gmail.com>
2020-01-09 12:11:49 +0100

    Skeleton - 064_namespaces_create.cpp

---
+++ 064_namespaces_create.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+void print_array(const int data[], int size)
+{
+  for (int i = 0; i < size, i++)
+  {
+    std::cout << data[i] << "\t";
+  }
+  std::cout << "\n";
+}
+
+int main()
+{
+  int data[] = {1, 2, 3};
+  int size = 3;
+
+  print_array(data, size);
+  std::cout << data[0] << std::endl;
+  
+  return 0;
+}
