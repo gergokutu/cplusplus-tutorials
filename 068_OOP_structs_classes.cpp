68db691a588ca366d2a6b889c402cd3b6cb8591a
gergokutu <gergo.kutu@gmail.com>
2020-01-10 16:47:24 +0100

    Intro - 068_OOP_structs_classes.cpp

---
+++ 068_OOP_structs_classes.cpp
@@ -0,0 +1,36 @@
+/*
+Intro to structs
+- the following concepts can be applied for classes as well
+- in other languages the diff between class & struct is much bigger...
+- diff between structs and classes is conventional >
+- people use struct for smaller and classes for bigger structures
+- usually use structs for Plain Old Data Structs (PODS) >
+  - inside the struct definition there is only data structs (variables)
+  - you can put methods but it's usually avoided by convention
+  - very simple variable based things...
+- transition is really easy between structs and classes
+
+Access modifiers:
+- public
+- private
+- protected (come up with inheritance)
+- members get access modifiers
+- you can change default access modifiers
+
+Public:
+- when creating an object (an instance)
+- public members can be accessed on that object
+- structs are members of public by default
+
+Private:
+- cannot be accessed on the object (instance)
+- but can be accessed on the class/struct
+- classes are members of pivate by default
+*/
+#include <iostream>
+
+int main()
+{
+  
+  return 0;
+}
