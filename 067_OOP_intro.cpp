b1bc8bff0238a5dee96cb5d42cee53a0cb4a497c
gergokutu <gergo.kutu@gmail.com>
2020-01-10 12:00:08 +0100

    OOP intro - 067_OOP_intro.cpp

---
+++ 067_OOP_intro.cpp
@@ -0,0 +1,36 @@
+/*
+Intro to Object Oriented Programming
+- pretty big topic
+- structs
+- all C-like programming languages has this concept >
+- like Java, C#...
+- but there is specifics to C++ OOP
+
+OOP:
+- based on classes and objects
+- classes the blueprints for its instances (objects)
+- we can describes data and real world things as objects
+- help to keep things organized
+- you can imagine it as an axcel table
+  - column names (the structure) > class
+  - each individual row (instances) > object
+
+Basic concepts of OOP:
+- ?data abstraction
+- structs
+  - similar to classes
+  - used for smaller things than classes
+- classes
+- members of classes
+  - data members (variables inside of a class)
+  - methods
+  - constructor
+  - destructor
+- encapsulation
+  - hide the inner details of the class
+  - give a public interface to interact
+- inheritance
+  - classes can inherit from other classes
+- polimorphism
+  - classes can be interpreted as their parent classes...
+*/
