67da4d1dda48b7dfa5f0198e66eb8a9fb1b92f66
gergokutu <gergo.kutu@gmail.com>
2020-01-10 18:23:33 +0100

    Intro - 069_OOP_classes_and_objects.cpp

---
+++ 069_OOP_classes_and_objects.cpp
@@ -0,0 +1,24 @@
+/*
+OOP - Classes and objects
+- very similar to structs
+
+Class:
+- blueprint
+- defines the shape of a data structure
+- cookie cutter example in the video...
+- process of going from a class to an object > instatiation
+- User eample in the video... User user1;
+- User the type (with class > building your own custom type)
+- constructor > spec function (method) wich creates a new user
+- inside the class with the same name > User()
+- automatically called everytime when a new user instantiated
+- do not have to do it like in JS > new User() > !!!NO-NO-NO!!!
+- we can define our own methods inside the class
+- data members (variable)
+- operator overloading (user1 == user2) > any other operator can be overloaded
+- static members
+  - describe the class in general
+  - stay associated with the class
+  - e.g number of users intatiated
+
+*/
