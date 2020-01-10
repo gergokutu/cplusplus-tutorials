63816d1607d8dd6390dc9e7d207af229870412c1
gergokutu <gergo.kutu@gmail.com>
2020-01-10 19:33:50 +0100

    Add user to users vector if userdoes not exists in the vector - 070_OOP_constructors.cpp

--- 068_OOP_structs_classes/main.cpp
+++ 068_OOP_structs_classes/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
+#include <vector>
 
-// create a class
-// everything is automatically private
-// encapsulation > hide diff variables inside the User (same with struct)
 class User
 {
   std::string status = "Gold";
 
-  // create public members
   public:
     std::string first_name;
     std::string last_name;
@@ -15,20 +12,84 @@ class User
     {
       return status;
     }
-  
- 
-  
 }; // do not forget the semi-colon
+
+int add_user_if_not_exists(std::vector<User> &users, User user)
+{
+  // iterate over all the elements of the vector
+  // and check if the passed data exists
+  for (int i = 0; i < users.size(); i++)
+  {
+    // if match
+    // we decide it
+    // e.g if the first_name and last_name equals, than they are the same
+    if (users[i].first_name == user.first_name &&
+        users[i].last_name == user.last_name)
+    { 
+      // return the index of the user
+      return i;
+    }
+  }
+  users.push_back(user);
+  // for the return type int
+  // return the lenght of the vector - 1
+  return users.size() - 1;
+}
+
+void print_vector(std::vector<User> users)
+{ 
+  // try with range based for loop
+  for (User user : users)
+  {
+    std::cout << user.first_name << "\n";
+  }
+}
 
 int main()
 {
-  User me;
-  me.first_name = "Gergo";
-  me.last_name = "Kovacs";
-  
-  std::cout << "First name:\t" << me.first_name << std::endl;
-  std::cout << "Last name:\t" << me.last_name << std::endl;
-  std::cout << "Status:\t" << me.get_status() << std::endl;
+  User user1, user2, user3;
+  user1.first_name = "Gergo";
+  user1.last_name = "Kovacs";
+
+  user2.first_name = "Dora";
+  user2.last_name = "Tonka";
+
+  user3.first_name = "Mogyi";
+  user3.last_name = "Pocok";
+
+  // create a vector of the users
+  // the type is User!
+  std::vector<User> users;
+  // add a user
+  users.push_back(user1);
+  // add a user
+  // intantiate to the default state
+  // users.push_back(User());
+  users.push_back(user2);
+  users.push_back(user3);
+
+  User user;
+  user.first_name = "Dora";
+  user.last_name = "Tonka";
+  // call the function
+  // it should not be added
+  // user index should be returned instead > 1
+  std::cout << add_user_if_not_exists(users, user) << std::endl;
+
+  User user4;
+  user.first_name = "David";
+  user.last_name = "Tonka";
+  // call the function
+  // it should be added
+  // its index should be returned > 3 (4th element... 0 index)
+  std::cout << add_user_if_not_exists(users, user4) << std::endl;
+  std::cout << users.size() << std::endl;
+
+  std::cout << users[0].first_name << std::endl;
+  // std::cout << users[1].first_name << std::endl; // gonna be empty string
+
+  std::cout << "\nAll first names in the vector:" << std::endl;
+  print_vector(users);
 
   return 0;
 }
--- 070_OOP_constructors.cpp
+++ 070_OOP_constructors.cpp
@@ -22,3 +22,98 @@ destructor:
   // code > e.g. a message, log sth...
 }
 */
+#include <iostream>
+#include <vector>
+
+class User
+{
+  std::string status = "Gold";
+
+  public:
+    std::string first_name;
+    std::string last_name;
+    std::string get_status()
+    {
+      return status;
+    }
+}; // do not forget the semi-colon
+
+int add_user_if_not_exists(std::vector<User> &users, User user)
+{
+  // iterate over all the elements of the vector
+  // and check if the passed data exists
+  for (int i = 0; i < users.size(); i++)
+  {
+    // if match
+    // we decide it
+    // e.g if the first_name and last_name equals, than they are the same
+    if (users[i].first_name == user.first_name &&
+        users[i].last_name == user.last_name)
+    { 
+      // return the index of the user
+      return i;
+    }
+  }
+  users.push_back(user);
+  // for the return type int
+  // return the lenght of the vector - 1
+  return users.size() - 1;
+}
+
+void print_vector(std::vector<User> users)
+{ 
+  // try with range based for loop
+  for (User user : users)
+  {
+    std::cout << user.first_name << "\n";
+  }
+}
+
+int main()
+{
+  User user1, user2, user3;
+  user1.first_name = "Gergo";
+  user1.last_name = "Kovacs";
+
+  user2.first_name = "Dora";
+  user2.last_name = "Tonka";
+
+  user3.first_name = "Mogyi";
+  user3.last_name = "Pocok";
+
+  // create a vector of the users
+  // the type is User!
+  std::vector<User> users;
+  // add a user
+  users.push_back(user1);
+  // add a user
+  // intantiate to the default state
+  // users.push_back(User());
+  users.push_back(user2);
+  users.push_back(user3);
+
+  User user;
+  user.first_name = "Dora";
+  user.last_name = "Tonka";
+  // call the function
+  // it should not be added
+  // user index should be returned instead > 1
+  std::cout << add_user_if_not_exists(users, user) << std::endl;
+
+  User user4;
+  user.first_name = "David";
+  user.last_name = "Tonka";
+  // call the function
+  // it should be added
+  // its index should be returned > 3 (4th element... 0 index)
+  std::cout << add_user_if_not_exists(users, user4) << std::endl;
+  std::cout << users.size() << std::endl;
+
+  std::cout << users[0].first_name << std::endl;
+  // std::cout << users[1].first_name << std::endl; // gonna be empty string
+
+  std::cout << "\nAll first names in the vector:" << std::endl;
+  print_vector(users);
+
+  return 0;
+}
