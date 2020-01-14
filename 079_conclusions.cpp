11e9c15f6c4e06b8e97ce77a732aec460d0251eb
gergokutu <gergo.kutu@gmail.com>
2020-01-14 12:44:15 +0100

    Summary, NS - 079_conclusions.cpp

---
+++ 079_conclusions.cpp
@@ -0,0 +1,14 @@
+/*
+Conclusions
+- got the fundamentals
+- directing to the next steps
+
+NS:
+- build real world apps
+- OOP deeper (design principles, multi inharitance...)
+- different types of collections
+- debugging (breakpoints...)
+- testing (Regression, Integration, Unit...)
+- concept of pointers and dynamic memory (it is more inportant in C but can come up in C++ to)
+- templetized classes (in generic style programming)
+*/
